a60893ad5067ca6dff436f2480f3ff68130c506d
yo4you <yo4you.nl@gmail.com>
2020-02-12 20:08:43 +0100

    player movement and attacking

---
+++ Assets/Scripts/PlayerAttackControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackControl : MonoBehaviour
+{
+	private Animator _animator;
+
+	private void Start()
+	{
+		_animator = GetComponent<Animator>();
+	}
+	void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			_animator.Play("light_hit");
+		}
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,31 +4,51 @@
 public class PlayerMovement : MonoBehaviour
 {
 	[SerializeField]
+	[Tooltip("the speed at which the player moves")]
 	private float _speed;
+	[SerializeField]
+	[Tooltip("the maximum time the player gets after attacking to change the direction")]
+	private float _attackDelay;
+	private float _attackDelayTimer = 0f;
 	private Animator _animator;
-	public bool LockMovement { get; set; }
+
+	
 
 	private void Start()
 	{
+		
 		_animator = GetComponent<Animator>();
 	}
 
 	private void Update()
 	{
+
+
 		var moveOffset = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-
-		if (Mathf.Abs(moveOffset.x) < 0.07f && Mathf.Abs(moveOffset.y) < 0.07f)
-		{
-			return;
-		}
-
 		if (moveOffset.magnitude > 1f)
 		{
 			moveOffset.Normalize();
 		}
-		_animator.SetFloat("x", moveOffset.x);
-		_animator.SetFloat("y", moveOffset.y);
+
+		if (!_animator.GetCurrentAnimatorStateInfo(0).IsName("Movement"))
+		{
+			if (_attackDelayTimer > 0f)
+			{
+				_attackDelayTimer -= Time.deltaTime;
+				RegisterMovementToAnimator(moveOffset);
+			}
+			return;
+		}
+		_attackDelayTimer = _attackDelay;
+		RegisterMovementToAnimator(moveOffset);
 
 		transform.Translate(_speed * Time.deltaTime * moveOffset);
+	}
+
+	private void RegisterMovementToAnimator(Vector3 moveOffset)
+	{
+		
+		_animator.SetFloat("x", moveOffset.x);
+		_animator.SetFloat("y", moveOffset.y);
 	}
 }
