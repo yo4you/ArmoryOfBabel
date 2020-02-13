e640616e361ccfb8ac49359006b5c4cb232309f9
yo4you <yo4you.nl@gmail.com>
2020-02-13 19:23:26 +0100

    basic setup to start generating rooms

---
+++ Assets/Scripts/DungeonGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGenerator : MonoBehaviour
+{
+	[SerializeField]
+	GameObject[] _prefabs;
+
+    void Start()
+    {
+		List<Vector3> placedPosition = new List<Vector3>();
+		var cellSize = GetComponent<Grid>().cellSize;
+
+		for (int i = 0; i < 5; i++)
+		{
+			var go = Instantiate(_prefabs[Random.Range(0,_prefabs.Length)], transform);
+			var roomData = go.GetComponent<RoomDataIdentifier>();
+			Vector3 size = roomData.GetWallSize();
+			int xAxis = Random.Range(0, 1);
+			size.x *= cellSize.x * xAxis;
+			size.y *= cellSize.y * 1-xAxis;
+			size *= Random.Range(0,1) == 0 ?
+		}
+    }
+}
---
+++ Assets/Scripts/RoomDataIdentifier.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class RoomDataIdentifier : MonoBehaviour
+{
+	enum Direction
+	{
+		NORTH,EAST,SOUTH,WEST
+	}
+
+	[SerializeField]
+	GameObject[] _doorways;
+	[SerializeField]
+	GameObject _walls;
+
+	public Vector3Int GetWallSize()
+	{
+		var tilemap = _walls.GetComponent<Tilemap>();
+		return tilemap.size;
+	}
+
+}
