fa45758dc1d10132007e40077c18efb2146a8e44
Jarvis van Zanten <yo4you.nl@gmail.com>
2020-06-30 17:37:25 +0200

    intro screen

---
+++ Assets/Scripts/InfoScreenBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoScreenBehaviour : MonoBehaviour
+{
+    static bool _shown = false;
+
+    void Start()
+    {
+        if (_shown)
+            Destroy(gameObject);
+        else
+            Time.timeScale = 0;
+        _shown = true;
+        
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown) 
+        {
+            Time.timeScale = 1;
+            Destroy(gameObject);
+        }
+    }
+}
